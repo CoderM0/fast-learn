2e8076dac869b75b3b0ddb9781ca9614772cfda6
CoderM0 <majdalfarfour@gmail.com>
2025-07-03 16:06:16 +0300

    fix quiz

--- resources/js/Pages/languages/QuizeHome.jsx
+++ resources/js/Pages/languages/QuizeHome.jsx
@@ -3,10 +3,11 @@ import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import QuestionField from "@/Components/QuestionField";
 import { useForm } from "@inertiajs/react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function QuizeHome({ quize, course_id, submit }) {
     // const [answers, setAnswers] = useState({});
-    const { data, setData, post, reset } = useForm();
+    const { data, setData, post, reset, processing } = useForm();
     console.log("keys", Object.keys(data).length == quize.questions.length * 2);
 
     const submitanswers = (e) => {
@@ -21,33 +22,43 @@ export default function QuizeHome({ quize, course_id, submit }) {
     return (
         <div>
             <div className="w-full px-2">
-                {quize.questions.map((quest) => {
-                    return (
-                        <div
-                            key={quest.id}
-                            className="bg-white p-2 rounded-xl my-2 border px-2"
-                        >
-                            <InputLabel className="text-blue-600 px-4 font-bold ">
-                                {quest.question_text}
-                            </InputLabel>
-                            <div className="flex justify-between my-5">
-                                {quest.options.map((op) => {
-                                    return (
-                                        <QuestionField
-                                            key={op.id}
-                                            op={op}
-                                            data={data}
-                                            questId={quest.id}
-                                            setData={setData}
-                                        />
-                                    );
-                                })}
+                {processing ? (
+                    <div className="w-full h-full flex flex-col gap-2  justify-center items-center mt-5">
+                        <AiOutlineLoading3Quarters
+                            className="text-green-500 animate-spin"
+                            size={"5rem"}
+                        />{" "}
+                        {"Submitting.. "}
+                    </div>
+                ) : (
+                    quize.questions.map((quest) => {
+                        return (
+                            <div
+                                key={quest.id}
+                                className="bg-white p-2 rounded-xl my-2 border px-2"
+                            >
+                                <InputLabel className="text-blue-600 px-4 font-bold ">
+                                    {quest.question_text}
+                                </InputLabel>
+                                <div className="flex justify-between my-5">
+                                    {quest.options.map((op) => {
+                                        return (
+                                            <QuestionField
+                                                key={op.id}
+                                                op={op}
+                                                data={data}
+                                                questId={quest.id}
+                                                setData={setData}
+                                            />
+                                        );
+                                    })}
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })
+                )}
 
-                {submit && (
+                {submit && !processing && (
                     <div className="flex gap-3 items-center">
                         <PrimaryButton
                             className="mt-6 w-full justify-center"
